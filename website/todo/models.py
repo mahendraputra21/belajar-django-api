39286754e62b2909f1db2df2adddcbac1e053103
Dewa Mahendra <31196162+mahendraputra21@users.noreply.github.com>
2022-12-13 16:49:05 +0800

    Add files via upload

---
+++ website/todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.TextField(max_length=200)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '%s: %s' % (self.id, self.title)
